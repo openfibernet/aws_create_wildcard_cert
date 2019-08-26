f86546ed0003f2928cebf82522602aa15eb2f2f8
Dan Aronson <dan.aronson@gmail.com>
2019-08-26 14:17:04 -0700

    fixes

--- import.py
+++ import.py
@@ -16,7 +16,6 @@ def randomString(stringLength=10):
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 
-dir_name = '/tmp/' + randomString(10)
 
 RENEWAL_GRACEPERIOD = os.environ.get('RENEWAL_GRACEPERIOD', 1)
 
@@ -33,7 +32,9 @@ else:
     s3_prefix = m.group(2)
     s3_client = boto3.client('s3')
 
+
 def run():
+    dir_name = '/tmp/' + randomString(10)
     logger.info('writing credentials file')
 
     arn = os.environ['CERTIFICATE_ARN']
@@ -53,7 +54,11 @@ def run():
         with open(credential_file, 'w') as file:
             file.write('dns_dnsimple_token = %s\n' % os.environ['CREDENTIAL'])
         os.chmod(credential_file, 0o600)
-        certbot.main.main(['certonly', '--staging', '--agree-tos', '--email', os.environ['EMAIL_ADDRESS'], '--non-interactive', '--dns-dnsimple', '--dns-dnsimple-credentials', credential_file, '--dns-dnsimple-propagation-seconds', '60', '-d', '*.' + domain_name, '--config-dir', dir_name, '--work-dir', dir_name, '--logs-dir', dir_name])
+        args = ['certonly', '--agree-tos', '--email', os.environ['EMAIL_ADDRESS'], '--non-interactive', '--dns-dnsimple', '--dns-dnsimple-credentials', credential_file, '--dns-dnsimple-propagation-seconds', '60', '-d', '*.' + domain_name, '--config-dir', dir_name, '--work-dir', dir_name, '--logs-dir', dir_name]
+        if os.environ.get('STAGING'):
+            args.append('--staging')
+        certbot.main.main(args)
+
         # certbot sets root logger level to DEBUG.... oy
         logging.getLogger().setLevel(logging.INFO)
 
@@ -69,7 +74,8 @@ def run():
         upload_to_s3('cert')
         upload_to_s3('chain')
         upload_to_s3('privkey')
-        #response = client.import_certificate(CertificateArn=arn, Certificate=cert, PrivateKey=privkey, CertificateChain=chain)
+        if not os.environ.get('STAGING'):
+            response = client.import_certificate(CertificateArn=arn, Certificate=cert, PrivateKey=privkey, CertificateChain=chain)
     else:
         logger.info('Not within grace period (%s day) of %s' % (RENEWAL_GRACEPERIOD, response['Certificate']['NotAfter']))
     return
